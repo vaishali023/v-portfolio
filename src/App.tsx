5bd69f48c101dd93709e7776db053d466f7b3659
Vaishali Chaudhary <vaishalichaudhary011@gmail.com>
2025-02-17 23:31:33 -0500

    Refactor: Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import './App.css'
 function App() {
   return (
    <div>
-    <h1>Hello World!</h1>
-   </div>
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>   </div>
   )
 }
 
