407e248644e647fabeca6a6efc59f09a3c7ef657
Vaishali Chaudhary <vaishalichaudhary011@gmail.com>
2025-03-17 23:41:59 -0400

    Feat: Create the header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,15 @@
 export default function Navbar() {
     return(
-      <header className="top-0">
-        <div></div>
-
+      <header className="top-0 flex flex-row">
+        <div> <h1>Portfolio</h1></div>
+        <div className=""></div>
+        <nav className="space-x-6">
+            <a href="" className="">Home</a>
+            <a href="" className="">About</a>
+            <a href="" className="">Stack</a>
+            <a href="" className="">Projects</a>
+            <a href="" className="">Contact</a>
+        </nav>
       </header>
     )
 }
