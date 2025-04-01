0d69e1b28221e088256ec69945ac8e42e288e21e
Vaishali Chaudhary <vaishalichaudhary011@gmail.com>
2025-03-31 23:08:49 -0400

    Refactor: Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+    return (
+        <footer className="bg-gray-900 text-white py-12">
+            <div className="container mx-auto">
+
+            </div>
+        </footer>
+    )
+}
