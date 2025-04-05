630f3d0eb9ca641eb35f4fda9affd080d06f0aef
Vaishali Chaudhary <vaishalichaudhary011@gmail.com>
2025-04-04 23:18:04 -0400

    Refactor: Add Home Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,11 @@
+import Home from "../components/Home";
 import Navbar from "../components/Navbar";
 
 export default function LandingPage() {
     return(
         <>
         <Navbar/>
+        <Home/>
         </>
     )
 }
